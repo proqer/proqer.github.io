e33129a41d08260b01713fd922bc0c05407e35f1
Maksim <39499391+proqer@users.noreply.github.com>
2018-11-30 22:02:58 +0200

    updated floyd

--- floyd/floyd.js
+++ floyd/floyd.js
@@ -26,73 +26,79 @@ const changeTable = () => {
 	table.innerHTML = res;
 }
 
-const solve = () => {
-	clearRecords();
-	let arr = new Array(size);
-	let pathArray = new Array(size);
+const getDataFromTable = () => {
+	let matrix = new Array(size);
 	for (let i = 0; i < size; i++) {
-		arr[i] = new Array(size);
-		pathArray[i] = new Array(size);
+		matrix[i] = new Array(size);
 	}
-
-	
 	let table = document.getElementById('table');
 	for (let i = 0; i < size; i++) {
 		for (let j = 0; j < size; j++) {
 			if (table.firstElementChild.children[i + 1].children[j + 1].firstElementChild.outerText == '∞') {
-				arr[i][j] = Infinity;
+				matrix[i][j] = Infinity;
 			} else {
-				arr[i][j] = +table.firstElementChild.children[i + 1].children[j + 1].firstElementChild.outerText;
+				matrix[i][j] = +table.firstElementChild.children[i + 1].children[j + 1].firstElementChild.outerText;
 			}
 		}
 	}
-	
+	return matrix;
+}
+
+const solve = () => {
+	clearRecords();
+	let distanceMatrix = getDataFromTable();
+
+	let pathMatrix = new Array(size);
+	for (let i = 0; i < size; i++) {
+		pathMatrix[i] = new Array(size);
+	}
 	for (let i = 0; i < size; i++) {
 		for (let j = 0; j < size; j++) {
 			if (i == j) {
-				pathArray[i][j] = ' - ';
-			} else if (arr[i][j] == Infinity) {
-				pathArray[i][j] = '( - )';
+				pathMatrix[i][j] = ' - ';
+			} else if (distanceMatrix[i][j] == Infinity) {
+				pathMatrix[i][j] = '( - )';
 			}
 			else {
-				pathArray[i][j] = `(x<sub>${i + 1}</sub>, x<sub>${j + 1}</sub>)`;
+				pathMatrix[i][j] = `(x<sub>${i + 1}</sub>, x<sub>${j + 1}</sub>)`;
 			}
 		}
 	}
 
-	addTable(arr, `<sup>${0}</sup>`);
-	addTable(pathArray, `<sub>${0}</sub>`);
+	addTable(distanceMatrix, `<sup>${0}</sup>`);
+	addTable(pathMatrix, `<sub>${0}</sub>`);
 	
-
+	let arr = distanceMatrix;
 	for (let m = 0; m < (size > 6 ? 6 : size); m++) {
+		addRecord([...Array(size)].map((item, it) => `d<sup>${m + 1}</sup><sub>${it + 1} ${it + 1}</sub>`).join(' = ') + ' = 0');
 		let arrNext = new Array(size);
-		let pathArrayNext = new Array(size);
+		let pathMatrixNext = new Array(size);
 		for (let i = 0; i < size; i++) {
 			arrNext[i] = new Array(size);
-			pathArrayNext[i] = new Array(size);
+			pathMatrixNext[i] = new Array(size);
 		}
 		for (let i = 0; i < size; i++) {
 			for (let j = 0; j < size; j++) {
 				if (i != j) {
 					if (arr[i][m] + arr[m][j] < arr[i][j]) {
-						pathArrayNext[i][j] = `(x<sub>${i + 1}</sub>, x<sub>${m + 1}</sub>) - (x<sub>${m + 1}</sub>, x<sub>${j + 1}</sub>)`;
 						arrNext[i][j] = arr[i][m] + arr[m][j];
+						pathMatrixNext[i][j] = `(x<sub>${i + 1}</sub>, x<sub>${m + 1}</sub>) - (x<sub>${m + 1}</sub>, x<sub>${j + 1}</sub>)`;
 					} else {
 						arrNext[i][j] = arr[i][j];
-						pathArrayNext[i][j] = pathArray[i][j];
+						pathMatrixNext[i][j] = pathMatrix[i][j];
 					}
-					addRecord(`d<sup>${m + 1}</sup><sub>${i + 1} ${j + 1}</sub> = min{d<sup>${m}</sup><sub>${i + 1} ${m + 1}</sub> + d<sup>${m}</sup><sub>${m + 1} ${j + 1}</sub>, d<sup>${m}</sup><sub>${i + 1} ${j + 1}</sub>} = min {${arr[i][m] == Infinity ? '&#8734;' : arr[i][m]} + ${arr[m][j] == Infinity ? '&#8734;' : arr[m][j]}, ${arr[i][j] == Infinity ? '&#8734;' : arr[i][j]}} = ${arrNext[i][j] == Infinity ? '&#8734;' : arrNext[i][j]}`);
+					addRecord(`d<sup>${m + 1}</sup><sub>${i + 1} ${j + 1}</sub> = min{d<sup>${m}</sup><sub>${i + 1} ${m + 1}</sub> + d<sup>${m}</sup><sub>${m + 1} ${j + 1}</sub>, d<sup>${m}</sup><sub>${i + 1} ${j + 1}</sub>} = min{${arr[i][m] == Infinity ? '&#8734;' : arr[i][m]} + ${arr[m][j] == Infinity ? '&#8734;' : arr[m][j]}, ${arr[i][j] == Infinity ? '&#8734;' : arr[i][j]}} = ${arrNext[i][j] == Infinity ? '&#8734;' : arrNext[i][j]}`);
 				}
 				else {
 					arrNext[i][j] = 0;
-					pathArrayNext[i][j] = ' - ';
+					pathMatrixNext[i][j] = ' - ';
 				}
 			}
 		}
 		addTable(arrNext, `<sup>${m + 1}</sup>`);
-		addTable(pathArrayNext, `<sub>${m + 1}</sub>`);
+		addTable(pathMatrixNext, `<sub>${m + 1}</sub>`);
 		arr = arrNext;
-		pathArray = pathArrayNext;
+		pathMatrix = pathMatrixNext;
 	}
 }
 
@@ -126,6 +132,6 @@ const addTable = (arr, m) => {
 		}
 		table += '</tr>';
 	}
-	table = `<a>D${m} = </a><table border="1"><tbody>${table}</tbody></table>`;
+	table = `<div style="display: flex; align-items: center; padding: 5px"><a style="margin-right: 5px">D${m} = </a><table border="1"><tbody>${table}</tbody></table></div>`;
 	result.innerHTML += table;
 }
